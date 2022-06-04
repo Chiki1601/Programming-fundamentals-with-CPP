6b2b902372d69412f0bfc244b4ca01c85bb6760f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-06-04 10:42:58 +0530

    Create if-Else.cpp

---
+++ Decision making statements/if-Else.cpp
@@ -0,0 +1,21 @@
+//if else in cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // clrscr();
+    int i;
+    cout<<"Press 0:";
+    cin>>i;
+    if(i==0){
+        cout<<"Zero is pressed!";
+    }
+    else{
+        cout<<"Zero is not pressed!";
+    }
+
+    //getch();
+}
