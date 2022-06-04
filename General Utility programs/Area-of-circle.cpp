03d345aa92a1e9b18cad1e2e829d5a7cc714ac8f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-06-04 10:44:26 +0530

    Create Area-of-circle.cpp

---
+++ General Utility programs/Area-of-circle.cpp
@@ -0,0 +1,16 @@
+//area fo circle
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int radius;
+    float area;
+    cout<<"Enter radius of a circle:";
+    cin>>radius;
+    area = 3.14 *radius *radius;
+    cout<<"Area fo circle:"<<area;
+
+    return 0;
+}
